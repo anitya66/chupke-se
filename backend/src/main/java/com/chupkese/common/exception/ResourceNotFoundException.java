7c6dce1b429b905659cbf4d282f5a3c3da996101
anitya66 <123goldirajput@gmail.com>
2026-08-20 20:33:28 +0530

    feat: add global exception handling

---
+++ backend/src/main/java/com/chupkese/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.chupkese.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
